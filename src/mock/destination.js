54cf536ccbb7bf1a2822c34bef53455cc37597cd
Ciberian <isi04047891@gmail.com>
2022-06-24 06:47:57 +0500

    Удаляет ненужные моковые данные

--- src/mock/destination.js
+++
@@ -1,4 +0,0 @@
-import { getRandomArrayElement } from '../util.js';
-import { destinations } from '../const.js';
-
-export const getDestination = () => getRandomArrayElement(destinations);
--- src/mock/offer.js
+++
@@ -1,3 +0,0 @@
-import { offers } from '../const.js';
-
-export const getOffers = () => offers;
